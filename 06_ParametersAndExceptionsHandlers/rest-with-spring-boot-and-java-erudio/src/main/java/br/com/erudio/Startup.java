8738f039ebf27945acacb12cb17918ee209436f2
wosh1ngton <woshington.rod@gmail.com>
2023-12-08 09:45:51 -0300

    alteracao

--- 06_ParametersAndExceptionsHandlers/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ 06_ParametersAndExceptionsHandlers/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -1,10 +1,8 @@
 package br.com.erudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Startup extends SpringBootServletInitializer {
@@ -13,22 +11,9 @@ public class Startup extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		
 		 
-		SpringApplication.run(Startup.class, args);		
+		SpringApplication.run(Startup.class, args);	
 
-//        Map<String, PasswordEncoder> encoders = new HashMap<>();
-//      
-//        Pbkdf2PasswordEncoder pbkdf2Encoder =
-//       		new Pbkdf2PasswordEncoder(
-//    				"", 8, 185000,
-//   				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-//       
-//        encoders.put("pbkdf2", pbkdf2Encoder);
-//        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-//        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-//      
-//        String result1 = passwordEncoder.encode("h4llh42602");
-//       
-//        System.out.println("My hash result1 " + result1);
+
    
 	}
 
