7dc060c46d5ba1a286a66e5fdc016cfaa2f89add
wosh1ngton <woshington.rod@gmail.com>
2023-12-10 14:22:28 -0300

    dockerizando a aplicação

--- 06_ParametersAndExceptionsHandlers/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ 06_ParametersAndExceptionsHandlers/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -2,7 +2,7 @@ package br.com.erudio.configs;
 
 public class TestConfigs {
 
-	public static final int SERVER_PORT = 8083;	
+	public static final int SERVER_PORT = 8888;	
 	public static final String HEADER_PARAM_ORIGIN = "Origin";
 	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
 	
